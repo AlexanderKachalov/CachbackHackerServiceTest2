61285acbdb2beba143af7822a79acba857fe3498
Alexander Kachalov <kachalow77@gmail.com>
2019-12-05 20:47:55 +0300

    @ParameterizedTest added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,53 +2,56 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturnZeroIfAmountEqual1000() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void shouldCalculateCashback(int amount, int expectedResult, String message) {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expectedResult = 0;
+
         int actualResult = service.remain(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
+
+        assertEquals(expectedResult, actualResult, message);
     }
 
-    @Test
-    void shouldReturn100IfAmountIs900() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int expectedResult = 100;
-        int actualResult = service.remain(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    void shouldReturn200IfAmountIs800() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 800;
-        int expectedResult = 200;
-        int actualResult = service.remain(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    void shouldReturn300IfAmountIs700() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 700;
-        int expectedResult = 300;
-        int actualResult = service.remain(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    void shouldReturn50IfAmountIs950() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 950;
-        int expectedResult = 50;
-        int actualResult = service.remain(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
+//    @Test
+//    void shouldReturn100IfAmountIs900() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 900;
+//        int expectedResult = 100;
+//        int actualResult = service.remain(amount);
+//        Assertions.assertEquals(expectedResult, actualResult);
+//    }
+//
+//    @Test
+//    void shouldReturn200IfAmountIs800() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 800;
+//        int expectedResult = 200;
+//        int actualResult = service.remain(amount);
+//        Assertions.assertEquals(expectedResult, actualResult);
+//    }
+//
+//    @Test
+//    void shouldReturn300IfAmountIs700() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 700;
+//        int expectedResult = 300;
+//        int actualResult = service.remain(amount);
+//        Assertions.assertEquals(expectedResult, actualResult);
+//    }
+//
+//    @Test
+//    void shouldReturn50IfAmountIs950() {
+//        CashbackHackService service = new CashbackHackService();
+//        int amount = 950;
+//        int expectedResult = 50;
+//        int actualResult = service.remain(amount);
+//        Assertions.assertEquals(expectedResult, actualResult);
+//    }
 }
